6a290c429c3ac9899cdd7014d8a1d6ac66292696
chltjdus27 <chltjdus27@gmail.com>
2018-05-05 16:38:58 +0900

    register user meal data

--- app/src/main/java/com/example/kate/personal_coach/FoodActivity.java
+++ app/src/main/java/com/example/kate/personal_coach/FoodActivity.java
@@ -2,8 +2,13 @@ package com.example.kate.personal_coach;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
@@ -28,19 +33,22 @@ import java.util.Map;
 
 public class FoodActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private Spinner mealTimeSpinner;
     public static DatabaseReference databaseReference;
     private FirebaseAuth mAuth;
     static FirebaseUser user = null;
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+
     private ArrayList<Food> foodList;
-    private AutoCompleteTextView foodNameAuto;
-    private FoodListAdapter adapter;
-    private ListView userMealList;
-    private TextView totalCalory;
-    private Food userFood;
-    private Button saveMealBtn;
+    private ArrayList<UserFood> userFoodList;
+
+    private UserFoodAdapter foodAdapter;
+    private TextView tv_totalCalory;
+    private Food food;
+    private UserFood userFood;
     private String mealTime, currDate;
-    private double totalCal;
+    private double totalCalory=0;
 
     private ValueEventListener getFoodList = new ValueEventListener(){
 
@@ -64,35 +72,14 @@ public class FoodActivity extends AppCompatActivity implements View.OnClickListe
 
         @Override
         public void onDataChange(DataSnapshot dataSnapshot) {
-            Map<String, Object> map = new HashMap<>();
 
             for (DataSnapshot snapshot: dataSnapshot.getChildren()) {
-                String key = snapshot.getKey();
-
-                if(!key.equals("total_kcal")){
-                    map.put(key, snapshot.getValue(Food.class));
-                    adapter.addFoodList(map);
-                }
+                UserFood uFood = snapshot.getValue(UserFood.class);
+                userFoodList.add(uFood);
+                totalCalory += uFood.getKcal();
             }
-            adapter.notifyDataSetChanged();
-        }
-
-        @Override
-        public void onCancelled(DatabaseError databaseError) {
-
-        }
-    };
-
-    private ValueEventListener getTotalCalory = new ValueEventListener(){
-
-        @Override
-        public void onDataChange(DataSnapshot dataSnapshot) {
-
-            if (dataSnapshot.getValue() == null){
-                totalCal = 0;
-            }else{
-                totalCal = dataSnapshot.getValue(Double.class);
-            }
+            tv_totalCalory.setText(totalCalory+"");
+            foodAdapter.notifyDataSetChanged();
         }
 
         @Override
@@ -106,6 +93,13 @@ public class FoodActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food);
 
+        mRecyclerView = (RecyclerView) findViewById(R.id.food_rc_view);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        foodAdapter = new UserFoodAdapter();
+        mRecyclerView.setAdapter(foodAdapter);
+
         databaseReference = FirebaseDatabase.getInstance().getReference();
         mAuth = FirebaseAuth.getInstance();
         user = mAuth.getCurrentUser();
@@ -114,28 +108,18 @@ public class FoodActivity extends AppCompatActivity implements View.OnClickListe
         setTimeSpinner();
 
         foodList = new ArrayList<>();
-        adapter = new FoodListAdapter();
+        userFoodList = new ArrayList<>();
 
-        userMealList = (ListView)findViewById(R.id.userMealList);
-        totalCalory = (TextView) findViewById(R.id.totalCalory);
-        userMealList.setAdapter(adapter);
+        tv_totalCalory = (TextView) findViewById(R.id.totalCalory);
 
         databaseReference.child("Food").addValueEventListener(getFoodList);
-        databaseReference.child("User_Food")
-                .child(user.getUid())
-                .child(currDate)
-                .addValueEventListener(getUserFoodList);
+        databaseReference.child("User_Food").child(user.getUid()).child(currDate).addValueEventListener(getUserFoodList);
 
-        databaseReference.child("User_Food")
-                .child(user.getUid())
-                .child(currDate).child("total_kcal")
-                .addValueEventListener(getTotalCalory);
-
-        saveMealBtn = (Button)findViewById(R.id.saveMealBtn);
+        Button saveMealBtn = (Button)findViewById(R.id.saveMealBtn);
         saveMealBtn.setOnClickListener(this);
-        totalCalory.setText(totalCal+"");
 
-        foodNameAuto = (AutoCompleteTextView)findViewById(R.id.foodNameAuto);
+        //키워드 자동완성
+        AutoCompleteTextView foodNameAuto = (AutoCompleteTextView)findViewById(R.id.foodNameAuto);
         foodNameAuto.setAdapter(new ArrayAdapter<Food>(
                 this,
                 android.R.layout.simple_dropdown_item_1line,
@@ -144,12 +128,13 @@ public class FoodActivity extends AppCompatActivity implements View.OnClickListe
         foodNameAuto.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                userFood = (Food) adapterView.getItemAtPosition(i);
+                food = (Food) adapterView.getItemAtPosition(i);
             }
         });
 
     }
 
+    //현재 날짜 구하기
     public void getCurrentDate(){
         long now = System.currentTimeMillis();
         Date date = new Date(now);
@@ -157,6 +142,7 @@ public class FoodActivity extends AppCompatActivity implements View.OnClickListe
         currDate = sdfNow.format(date);
     }
 
+    //현재 시간 구하기
     public String currentTime(){
         long now = System.currentTimeMillis();
         Date date = new Date(now);
@@ -174,7 +160,7 @@ public class FoodActivity extends AppCompatActivity implements View.OnClickListe
                 "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00", "24:00"
         };
 
-        mealTimeSpinner = (Spinner)findViewById(R.id.mealTimeSpinner);
+        Spinner mealTimeSpinner = (Spinner)findViewById(R.id.mealTimeSpinner);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(
                 this,
                 android.R.layout.simple_spinner_dropdown_item,
@@ -199,15 +185,60 @@ public class FoodActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
         // 음식정보 저장
-        databaseReference.child("User_Food")
-                .child(user.getUid())
-                .child(currDate).child(mealTime)
-                .setValue(userFood);
+        userFood = new UserFood();
+        userFood.setTime(currDate+" "+mealTime);
+        userFood.setKcal(food.getKcal());
+        userFood.setName(food.getName());
 
-        databaseReference.child("User_Food")
-                .child(user.getUid())
-                .child(currDate).child("total_kcal")
-                .setValue(totalCal+userFood.getKcal());
+        databaseReference.child("User_Food").child(user.getUid()).child(currDate).child(mealTime).setValue(userFood);
 
+        //adapter 갱신
+        userFoodList.clear();
+        totalCalory=0;
+        foodAdapter.notifyDataSetChanged();
+    }
+
+
+
+    class UserFoodAdapter extends RecyclerView.Adapter<UserFoodAdapter.ViewHolder>{
+
+
+        @Override
+        public UserFoodAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_food, parent, false);
+
+            return new UserFoodAdapter.ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(UserFoodAdapter.ViewHolder holder, int position) {
+            holder.tv_food_name.setText(String.valueOf(userFoodList.get(position).getName()));
+            holder.tv_kcal.setText(String.valueOf(userFoodList.get(position).getKcal())+" kcal");
+            holder.tv_timeInfo.setText(String.valueOf(userFoodList.get(position).getTime()));
+        }
+
+
+
+        @Override
+        public int getItemCount() {
+            return userFoodList.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+
+            TextView tv_food_name;
+            TextView tv_kcal;
+            TextView tv_timeInfo;
+            CardView cv;
+
+            public ViewHolder(View view) {
+                super(view);
+                tv_food_name = (TextView) view.findViewById(R.id.tv_food_name);
+                tv_kcal = (TextView) view.findViewById(R.id.tv_kcal);
+                tv_timeInfo = (TextView) view.findViewById(R.id.tv_timeInfo);
+                cv = (CardView) view.findViewById(R.id.cv_food);
+
+            }
+        }
     }
 }
